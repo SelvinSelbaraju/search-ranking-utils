bc65749ef2fb84aab380b89f1a04e400c4e2b9aa
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2024-07-25 16:07:44 +0100

    add dummy data and test fixture

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import os
+import pytest
+import pandas as pd
+
+"""
+Contains reusable artifacts for tests like data and config
+"""
+
+LOCAL_DIRECTORY = os.path.dirname(__file__)
+DUMMY_CSV_PATH = os.path.join(LOCAL_DIRECTORY, "data", "dummy_data.csv")
+
+@pytest.fixture(scope="session")
+def dummy_df() -> pd.DataFrame:
+    df = pd.read_csv(DUMMY_CSV_PATH)
+    return df
