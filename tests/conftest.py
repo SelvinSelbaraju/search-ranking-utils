61bbc4d5ca9f60ec7bf5882223c65b5231314398
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2024-07-25 16:09:16 +0100

    run pre-commit hooks on files

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ Contains reusable artifacts for tests like data and config
 LOCAL_DIRECTORY = os.path.dirname(__file__)
 DUMMY_CSV_PATH = os.path.join(LOCAL_DIRECTORY, "data", "dummy_data.csv")
 
+
 @pytest.fixture(scope="session")
 def dummy_df() -> pd.DataFrame:
     df = pd.read_csv(DUMMY_CSV_PATH)
