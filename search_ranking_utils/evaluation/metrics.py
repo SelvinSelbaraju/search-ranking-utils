b5a00710607c98f6671de8cff88c721ea764a0ed
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2024-07-26 14:45:19 +0100

    calculate ranking metrics

--- search_ranking_utils/evaluation/metrics.py
+++ search_ranking_utils/evaluation/metrics.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Dict
+import numpy as np
 import pandas as pd
 from importlib import import_module
 
@@ -10,7 +11,7 @@ def get_pointwise_metrics(
     schema: dict,
     y_pred_col: str,
     pointwise_metrics: List[str],
-):
+) -> Dict[str, float]:
     """
     Calculate all of the desired pointwise metrics on y_true and y_pred
     """
@@ -18,4 +19,53 @@ def get_pointwise_metrics(
     for metric in pointwise_metrics:
         metric_func = getattr(SKLEARN_METRICS_MODULE, metric)
         metrics[metric] = metric_func(df[schema["target"]], df[y_pred_col])
+    return metrics
+
+
+def calculate_query_ranking_metric(
+    y_true: pd.Series, y_pred: pd.Series, ranking_metric_name: str
+) -> float:
+    """
+    For a single query, calculate a ranking metric
+    Needs to be done query by query as variable length queries
+    Each pd.Series contains an array of actual/predicted scores
+    Then average the query scores at the end
+    """
+    metric_func = getattr(SKLEARN_METRICS_MODULE, ranking_metric_name)
+    query_metrics = np.array([])
+    if len(y_true) != len(y_pred):
+        raise ValueError("y_true and y_pred must be the same length")
+    for i in range(len(y_true)):
+        # Needs to be 2-D for ranking metric calculation
+        y_true_arr = np.array(y_true[i]).reshape(1, -1)
+        y_pred_arr = np.array(y_pred[i]).reshape(1, -1)
+        query_metrics = np.append(
+            query_metrics, metric_func(y_true_arr, y_pred_arr)
+        )
+    return query_metrics.mean()
+
+
+def get_ranking_metrics(
+    df: pd.DataFrame,
+    schema: dict,
+    y_pred_col: str,
+    ranking_metrics: List[str],
+) -> Dict[str, float]:
+    metrics = {}
+    y_true_queries = df.groupby(schema["query_col"])[schema["target"]].agg(
+        list
+    )
+    # Remove all queries of size 1 as can't rank
+    # Also remove any queries which had no relevant items
+    drop_indices = (y_true_queries.apply(lambda x: len(x) > 1)) & (
+        y_true_queries.apply(lambda x: sum(x) > 0)
+    )
+    y_true_queries = y_true_queries[drop_indices]
+    y_pred_queries = df.groupby(schema["query_col"])[y_pred_col].agg(list)[
+        drop_indices
+    ]
+    for metric in ranking_metrics:
+        metrics[metric] = calculate_query_ranking_metric(
+            y_true_queries, y_pred_queries, metric
+        )
     return metrics
--- tests/search_ranking_utils/evaluation/test_metrics.py
+++ tests/search_ranking_utils/evaluation/test_metrics.py
@@ -1,29 +1,45 @@
 import pytest
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from search_ranking_utils.utils.testing import assert_dicts_equal
 from search_ranking_utils.preprocessing.data_preprocessing import split_dataset
 from search_ranking_utils.modelling.model_factory import ModelFactory
-from search_ranking_utils.evaluation.metrics import get_pointwise_metrics
+from search_ranking_utils.evaluation.metrics import (
+    get_pointwise_metrics,
+    calculate_query_ranking_metric,
+    get_ranking_metrics,
+)
 
 
 @pytest.fixture
-def trained_model(dummy_trainable_df, dummy_schema) -> LogisticRegression:
+def predicted_df(dummy_trainable_df, dummy_schema) -> pd.DataFrame:
     _, X, y = split_dataset(dummy_trainable_df, dummy_schema)
     model = ModelFactory.get_instance_from_cofing(
         "sklearn.linear_model:LogisticRegression"
     )
     model.fit(X, y)
-    return model
-
-
-def test_get_pointwise_metrics(
-    dummy_trainable_df, dummy_schema, trained_model
-):
     df, X, y = split_dataset(dummy_trainable_df, dummy_schema)
-    df["prediction"] = pd.Series(trained_model.predict_proba(X)[:, 1])
+    df["prediction"] = pd.Series(model.predict_proba(X)[:, 1])
+    return df
+
+
+def test_get_pointwise_metrics(predicted_df, dummy_schema):
     metrics = get_pointwise_metrics(
-        df, dummy_schema, "prediction", ["roc_auc_score", "log_loss"]
+        predicted_df, dummy_schema, "prediction", ["roc_auc_score", "log_loss"]
     )
     expected_metrics = {"roc_auc_score": 0.8, "log_loss": 0.5122374154189449}
+    assert_dicts_equal(expected_metrics, metrics)
+
+
+def test_calculate_query_ranking_metric():
+    y_true = pd.Series([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0]])
+    y_pred = pd.Series([[0.8, 0.6, 0.7], [0.9, 0.95, 0.3, 0.4]])
+    ndcg = calculate_query_ranking_metric(y_true, y_pred, "ndcg_score")
+    assert ndcg == 1.0
+
+
+def test_get_ranking_metrics(predicted_df, dummy_schema):
+    metrics = get_ranking_metrics(
+        predicted_df, dummy_schema, "prediction", ["ndcg_score"]
+    )
+    expected_metrics = {"ndcg_score": 0.8154648767857287}
     assert_dicts_equal(expected_metrics, metrics)
