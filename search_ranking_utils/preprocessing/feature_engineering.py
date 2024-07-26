ba84004d843f159d9722b5d78d8760f54642d123
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2024-07-26 16:48:44 +0100

    get date part from a timestamp

--- search_ranking_utils/preprocessing/feature_engineering.py
+++ search_ranking_utils/preprocessing/feature_engineering.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sentence_transformers import SentenceTransformer, util
 
 EMBEDDING_MODEL = "msmarco-MiniLM-L-6-v3"
@@ -13,3 +14,21 @@ def calculate_text_cosine_similarity(text_1: str, text_2: str) -> float:
     embedding_1 = model.encode(text_1)
     embedding_2 = model.encode(text_2)
     return util.cos_sim(embedding_1, embedding_2)
+
+
+def get_timestamp_part(
+    timestamp_str: str,
+    timestamp_part: str,
+    timestamp_format: str = "%Y-%m-%d %H:%M:%S",
+) -> int:
+    """
+    Given a timestamp and the format, get a part of it
+    Eg. Get what the hour is or the day of the week
+    Monday is 0 indexed
+    """
+    dt = datetime.strptime(timestamp_str, timestamp_format)
+    # Weekday is a method not an attribute
+    if timestamp_part == "weekday":
+        return dt.weekday()
+    else:
+        return getattr(dt, timestamp_part)
--- tests/search_ranking_utils/preprocessing/test_feature_engineering.py
+++ tests/search_ranking_utils/preprocessing/test_feature_engineering.py
@@ -1,6 +1,7 @@
 import pytest
 from search_ranking_utils.preprocessing.feature_engineering import (
     calculate_text_cosine_similarity,
+    get_timestamp_part,
 )
 
 
@@ -14,3 +15,16 @@ from search_ranking_utils.preprocessing.feature_engineering import (
 def test_calculate_text_cosine_similarity(text_1, text_2):
     score = calculate_text_cosine_similarity(text_1, text_2)
     assert score > -1 and score < 1
+
+
+@pytest.mark.parametrize(
+    argnames=("timestamp_str", "timestamp_part", "expected"),
+    argvalues=(
+        ["2024-07-01 01:00:00", "hour", 1],
+        ["1999-12-06 12:56:45", "second", 45],
+        ["2024-07-26 12:56:45", "weekday", 4],
+    ),
+)
+def test_get_timestamp_part(timestamp_str, timestamp_part, expected):
+    result = get_timestamp_part(timestamp_str, timestamp_part)
+    assert result == expected
