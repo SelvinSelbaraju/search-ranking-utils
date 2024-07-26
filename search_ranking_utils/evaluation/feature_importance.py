2f9c913577aa5280224f965eaf93561230aee84b
Selvin Selbaraju <55618467+SelvinSelbaraju@users.noreply.github.com>
2024-07-26 15:49:07 +0100

    Model Evaluation  (#5)
    * calculate pointwise metrics
    
    * calculate ranking metrics
    
    * calculate feature importance
    
    * add comment

---
+++ search_ranking_utils/evaluation/feature_importance.py
@@ -0,0 +1,38 @@
+from typing import Tuple
+import numpy as np
+import pandas as pd
+import shap
+from search_ranking_utils.preprocessing.data_preprocessing import split_dataset
+
+
+def calculate_feature_importance(
+    df: pd.DataFrame, schema: dict, model
+) -> Tuple[np.ndarray, pd.DataFrame]:
+    """
+    Given a preprocessed dataset, calcualte feature importance
+    """
+    df, X, y = split_dataset(df, schema)
+
+    # Use Shap kernel predict by defining inner function
+    def f(X):
+        return model.predict_proba(X)[:, 1]
+
+    # Used as the background values to sample from
+    background = _generate_background(X)
+
+    explainer = shap.Explainer(f, background)
+    shap_values = explainer(X).values
+    shap_df = pd.DataFrame(shap_values, columns=X.columns)
+    # Aggregate per row feature importance
+    shap_df = shap_df.mean().sort_values(ascending=False)
+    return shap_values, shap_df
+
+
+def _generate_background(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculate background samples using medians
+    Assumes df is preprocessed, no categorical features
+    """
+    background = df.head(1)
+    background[df.columns] = df.median()
+    return background
